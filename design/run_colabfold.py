5f80bc2deb535537de2c48ecd12d086697a3a278
Jeffrey Chang <jeffjar.chang@gmail.com>
2023-12-06 10:50:58 -0500

    Example colabfold script

---
+++ design/run_colabfold.py
@@ -0,0 +1,52 @@
+'''
+Example script for using colabfold
+
+Input: a .fasta of sequences (or a directory, or a .a3m file, etc.)
+Parameters: see the output of `colabfold_batch --help`
+Output: a directory of pdbs of predicted structures
+
+- jchang 2023-12-06
+'''
+from pathlib import Path
+import subprocess
+
+COLABFOLD_EXEC = '/nfs/sbgrid/programs/x86_64-linux/colabfold/1.5.2/bin/colabfold_batch'
+
+def run_colabfold(input_file : str,
+                  output_dir : str,
+                  flags_list : list = [],
+                  gpu_id : int = None,
+                  colabfold_exec : str = COLABFOLD_EXEC,
+                  verbose : bool = False):
+    '''
+    Parameters:
+        input_file          .fasta of sequences (or directory, .a3m file, etc.)
+        output_dir          directory for where to write the output
+        flags_list          list of parameters to pass into colabfold, e.g.
+                            [ '--msa-mode', 'single_sequence', '--overwrite-existing-results']
+
+    Optional parameters:
+        gpu_id              if provided, runs on the GPU with this id
+        colabfold_exec      what executable to run
+        verbose             whether to show what commmand you're running and the output
+
+    Returns: 
+        None
+    '''
+    gpu_env_vars = {"CUDA_DEVICE_ORDER": "PCI_BUS_ID"}
+    if gpu_id is not None:
+        gpu_env_vars["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
+    cmd_parts = [ str(colabfold_exec), str(input_file), str(output_dir) ] + flags_list
+
+    if verbose:
+        print(' '.join(cmd_parts))
+        subprocess.run(cmd_parts, env=gpu_env_vars)
+    else:
+        subprocess.run(cmd_parts, env=gpu_env_vars, stdout=subprocess.DEVNULL)
+
+if __name__=='__main__':
+    run_colabfold('colabfold_input/2w9r_seqs.fasta',
+                  'colabfold_output/pdb',
+                  ['--msa-mode', 'single_sequence'],
+                  gpu_id=0,
+                  verbose=True)
