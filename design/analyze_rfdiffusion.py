c84bd77a5133c9872694e4043df06aa919aa434a
Jeffrey Chang <jeffjar.chang@gmail.com>
2023-12-06 14:42:45 -0500

    Updates to analyze rfdiffusion

--- design/analyze_rfdiffusion.py
+++ design/analyze_rfdiffusion.py
@@ -2,10 +2,8 @@
 Analyze RFDiffusion outputs
 
 Input:
-    .pdb of RFDiffusion generated scaffold
+    .pdb (and .trb) of RFDiffusion generated scaffold
 Optional inputs:
-    .pdb for any motifs used as input to RFDiffusion
-    contig string for the motif
     .log file of the RFDiffusion output
 Output:
     dict with information such as radius of gyration, dssp string, etc.
@@ -18,6 +16,7 @@ import prody as pr
 from scipy.spatial.distance import cdist
 import re
 import json
+from collections import defaultdict
 
 
 class ContigStringParseException(Exception):
@@ -43,16 +42,16 @@ def parse_contig_string(contig_str: str):
         '19-19/E8-10/26-26/E30-33/37-37/E78-81/17-17/0'
     Example output:
         [
-            {'contig': 'E8-10', 'start': 20, 'end': 22}, 
-            {'contig': 'E30-33', 'start': 49, 'end': 52}, 
-            {'contig': 'E78-81', 'start': 90, 'end': 93}
+            {'contig': 'E8-10', 'resnum_start': 20, 'resnum_end': 22}, 
+            {'contig': 'E30-33', 'resnum_start': 49, 'resnum_end': 52}, 
+            {'contig': 'E78-81', 'resnum_start': 90, 'resnum_end': 93}
         ]
     '''
     curr = 1
     pieces = []
     for part in contig_str.split('/'):
         if part == '0':
-            # this is the end
+            # this is the resnum_end
             break
 
         if part[0].isdigit():
@@ -67,8 +66,8 @@ def parse_contig_string(contig_str: str):
             length = m_end - m_start + 1
             pieces.append({
                 'contig' : part,
-                'start' : curr,
-                'end' : curr + length - 1,
+                'resnum_start' : curr,
+                'resnum_end' : curr + length - 1,
             })
             curr += length
 
@@ -92,7 +91,7 @@ def extract_info_from_log(log_filename):
 
     return all_info
 
-def extract_info_from_trb(trb_filename, designed_chains=None):
+def extract_info_from_trb(trb_filename):
     '''Returns dict of relevant information from the .trb file output by RFDiffusion'''
     trb = np.load(trb_filename, allow_pickle=True)
 
@@ -104,24 +103,24 @@ def extract_info_from_trb(trb_filename, designed_chains=None):
     # sampled_mask contains the spacer lengths actually sampled
     # we want to convert this into actual residue numbers
     # example: ['19-19/E8-10/26-26/E30-33/37-37/E78-81/17-17/0', 'G0-5/0']
-    sampled_mask = {'ABCDEFGH'[i] : s for i,s in enumerate(trb['sampled_mask']) }
+    sampled_mask = trb['sampled_mask']
     chains = dict()
-    start = 1
-    for chain,contig_str in sampled_mask.items():
+    resnum_start = 1
+    for i,contig_str in enumerate(sampled_mask):
         pieces, length = parse_contig_string(contig_str)
-        if designed_chains is None or chain in designed_chains:
-            chains[chain] = {
-                'start' : start,
-                'end' : start + length - 1,
-                'length' : length,
-                'pieces' : pieces,
-            }
-        start += length
+        chain = 'ABCDEFGH'[i]
+        chains[chain] = {
+            'contig' : contig_str,
+            'resnum_start' : resnum_start,
+            'resnum_end' : resnum_start + length - 1,
+            'length' : length,
+            'pieces' : pieces,
+        }
+        resnum_start += length
 
     return {
         'overall_plddt' : float(plddt.mean()),
         'motif_plddt' : float(plddt[mask_motif].mean()),
-        'sampled_mask' : sampled_mask,
         'chains' : chains,
     }
 
@@ -158,7 +157,7 @@ def parse_dssp(dssp_out_filepath):
     return dssp_ss
 
 
-def calculate_dssp_str(scaffold_path, dssp_filename, chains=None):
+def calculate_dssp_str(scaffold_path, dssp_filename):
     '''
     scaffold_path:      pdb to run dssp for
     dssp_filename:      where the dssp output goes
@@ -174,10 +173,7 @@ def calculate_dssp_str(scaffold_path, dssp_filename, chains=None):
     dssp_dict = parse_dssp(dssp_filename)
 
     # get the dssp str for each chain separately
-    if chains is None:
-        # if the user does not specify a chain, use all chains
-        chains = sorted(set(chain for chain,resnum in dssp_dict.keys()))
-
+    chains = sorted(set(chain for chain,resnum in dssp_dict.keys()))
     dssp_strs = dict()
     for chain in chains:
         resnums = sorted(resnum for c, resnum in dssp_dict.keys()
@@ -198,7 +194,10 @@ def calculate_contact_order(ca_coords, skip_number=2, contact_distance=8):
     dm = cdist(ca_coords, ca_coords)
     i_resinds, j_resinds = np.where(np.triu(dm <= contact_distance, k=1))
     seq_seps = [np.abs(i-j) for i, j in zip(i_resinds, j_resinds) if np.abs(i-j) > skip_number]
-    co = sum(seq_seps) / (len(ca_coords) * len(seq_seps))
+    try:
+        co = sum(seq_seps) / (len(ca_coords) * len(seq_seps))
+    except ZeroDivisionError:
+        co = np.nan
     return co
 
 def longest_run(s, c):
@@ -217,39 +216,75 @@ def longest_run(s, c):
 def analyze_rfdiffusion_output(
         scaffold_path : str,
         calculate_dssp : bool = True,
+        log_filename : str = None,
         output_json : str = None,
         designed_chains : list = None
         ):
     '''
+    Parses the .pdb and .trb file outputted by RFDiffusion
+        (the .trb file is assumed to have the same stem as the .pdb file)
+
     Parameters:
         scaffold_path           .pdb filepath of the scaffold created by RFDiffusion
 
     Optional parameters:
         calculate_dssp      whether to run dssp (default: True)
+        log_filename        if provided, extra fields are also parsed from the .log file of RFDiffusion output
+                                (make sure you provide the correct log file!)
         output_json         if provided, the parsed rfdiffusion output is written here as a .json
         designed_chains     which chains were designed, e.g. ['A'] (default: all chains)
 
     Returns: 
         dict with the following fields
+            pdb_path
+            pdb_stem
             overall_plddt
             motif_plddt
-            sampled_mask
+            seq                     (only if log_filename is provided)
+            motif_rmsd              (only if log_filename is provided)
+            chains
+                contig
+                resnum_start
+                resnum_end
+                length
+                dssp_str            (only for designed chains)
+                contact_order       (only for designed chains)
+                radius_of_gyration  (only for designed chains)
     '''
     trb_file = str(scaffold_path).replace('.pdb', '.trb')
     dssp_filename = str(scaffold_path).replace('.pdb', '.dssp')
 
-    scaffold = pr.parsePDB(scaffold_path)
-    if designed_chains is None:
-        ca_coords = scaffold.select('name CA').getCoords()
-    else:
-        ca_coords = scaffold.select(f'chain {" ".join(designed_chains)} name CA').getCoords()
+    info_dict = dict()
+    stem = Path(scaffold_path).stem
+    info_dict['scaffold_stem'] = stem
+    info_dict['scaffold_path'] = str(Path(scaffold_path).resolve())
 
     # Parse trb file
-    info_dict = extract_info_from_trb(trb_file, designed_chains=designed_chains)
+    info_dict.update(extract_info_from_trb(trb_file))
+
+    # Parse log file
+    if log_filename is not None:
+        info_dict.update(extract_info_from_log(log_filename).get(stem, dict()))
+
+    scaffold = pr.parsePDB(scaffold_path)
+    if designed_chains is None:
+        designed_chains = list(sorted(set(scaffold.getChids())))
+
     if calculate_dssp:
-        info_dict['dssp_str'] = calculate_dssp_str(scaffold_path, dssp_filename, chains=designed_chains)
-    info_dict['contact_order'] = calculate_contact_order(ca_coords)
-    info_dict['radius_of_gyration'] = calculate_radius_of_gyration(ca_coords)
+        dssp_dict = calculate_dssp_str(scaffold_path, dssp_filename)
+    else:
+        dssp_dict = dict()
+
+    for chain in designed_chains:
+        scaffold_chain_ca = scaffold.select(f'chain {chain} name CA')
+        if scaffold_chain_ca is None or len(scaffold_chain_ca) == 0:
+            raise ValueError(f'Chain {chain} not found in {scaffold_path}')
+        ca_coords = scaffold_chain_ca.getCoords()
+
+        chain_dict = info_dict['chains'][chain]
+        chain_dict['dssp_str'] = dssp_dict.get(chain, '')
+        chain_dict['contact_order'] = calculate_contact_order(ca_coords)
+        chain_dict['radius_of_gyration'] = calculate_radius_of_gyration(ca_coords)
 
     if output_json is not None:
         Path(output_json).parent.mkdir(exist_ok=True, parents=True)
@@ -261,6 +296,7 @@ def analyze_rfdiffusion_output(
 if __name__=='__main__':
     analyze_rfdiffusion_output(
         'rfdiffusion_output/pdb/3dnj_0.pdb',
+        log_filename='rfdiffusion_output/rfdiff.log',
         output_json='rfdiffusion_output/3dnj_0.json',
-        designed_chains='A',
+        designed_chains=['A']
     )
